2ca43eafe5f6baf1366d62b0efc8a126bcfc6aac
AlfonsoAltamiranoLeal <alfonso.altamirano@bambu-mobile.com>
2024-07-19 00:08:40 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTabsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(): void {
+    if (this.isPlatformBrowser()) {
+      this.mobile = this.isMobile();
+    }
+  }
+  public mobile = false;
   title = 'paola-shiadani-pianista';
+  public width = 640;
+  public height = 360;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.isPlatformBrowser()) {
+      this.mobile = this.isMobile();
+    }
+  }
+
+  public isMobile(): boolean {
+    if (this.isPlatformBrowser()) {
+      const sizeW = window.innerWidth;
+      const sizeH = window.innerHeight;
+      return sizeW <= 742 || sizeH <= 450;
+    }
+    return false;
+  }
+
+  private isPlatformBrowser(): boolean {
+    return typeof window !== 'undefined';
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        wine: "#800020",
+        crimson: "#DC143C",
+        gold: "#FFD700",
+        ebony: "#0C0C0C",
+        pearl: "#FDFDFD",
+        silver: "#C0C0C0",
+        navy: "#000080",
+      },
+    },
+  },
+  plugins: [],
+};
